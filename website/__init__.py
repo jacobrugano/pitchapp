5b2f1c86a7386539dc79f1f236fd588bcfd63900
github_username <jacobrugano@gmail.com>
2022-03-12 15:04:27 +0300

    Added secret key details

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fapapvkivnsixq:8ef7a724f90c82b8f262300f38ad6bd9a92424f88626a2f038def1bc9abc5eb0@ec2-34-231-183-74.compute-1.amazonaws.com:5432/d7f6f2p99tqmbv'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fapapvkivnsixq:8ef7a724f90c82b8f262300f38ad6bd9a92424f88626a2f038def1bc9abc5eb0@ec2-34-231-183-74.compute-1.amazonaws.com:5432/d7f6f2p99tqmbv'
     db.init_app(app)
 
     from .views import views
